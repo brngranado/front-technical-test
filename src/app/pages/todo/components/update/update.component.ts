d276318cce4d837325de40501cc254b01ff90f24
Bryan Granado <68772240+brngranado@users.noreply.github.com>
2024-06-12 11:43:25 -0400

    ADD: todo app done

--- src/app/pages/todo/components/update/update.component.ts
+++ src/app/pages/todo/components/update/update.component.ts
@@ -1,12 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  NonNullableFormBuilder,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { TaskService } from '../../../../services/task.service';
+import { CommonModule } from '@angular/common';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzDrawerModule } from 'ng-zorro-antd/drawer';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 
 @Component({
   selector: 'app-update',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    NzLayoutModule,
+    NzDrawerModule,
+    NzFormModule,
+    ReactiveFormsModule,
+    NzButtonModule,
+  ],
   templateUrl: './update.component.html',
-  styleUrl: './update.component.scss'
+  styleUrl: './update.component.scss',
 })
 export class UpdateComponent {
+  @Input() name: string = '';
+  @Input() id: string = '';
 
+  taskUpdate: FormGroup<{
+    name: FormControl<string>;
+  }> = this.fb.group({
+    name: ['', [Validators.required]],
+  });
+
+  visible = false;
+
+  open(): void {
+    this.visible = true;
+  }
+
+  close(): void {
+    this.visible = false;
+  }
+
+  submitForm(): void {
+    console.log('submit', this.taskUpdate.value, this.id);
+    if (this.taskUpdate.valid) {
+      this.taskService.update({
+        id: this.id,
+        name: this.taskUpdate.value.name || '',
+      });
+      this.close();
+    } else {
+      Object.values(this.taskUpdate.controls).forEach((control) => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
+    }
+  }
+  constructor(
+    private taskService: TaskService,
+    private fb: NonNullableFormBuilder
+  ) {
+    this.taskUpdate.get('name')?.setValue(this.name);
+  }
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IUser } from './interfaces';
@@ -17,5 +17,34 @@ export class ApiService extends HttpClient {
 
   register(body: IUser): Observable<any> {
     return this.post('http://localhost:9200/api/auth/signup', body);
+  }
+  getTasks(): Observable<any> {
+    const token = sessionStorage.getItem('token');
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+    return this.get('http://localhost:9200/api/tasks', { headers });
+  }
+  createTask(body: any): Observable<any> {
+    const token = sessionStorage.getItem('token');
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+    return this.post('http://localhost:9200/api/tasks', body, { headers });
+  }
+  updateTask(id: string, body: any): Observable<any> {
+    const token = sessionStorage.getItem('token');
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+    return this.put(`http://localhost:9200/api/tasks/${id}`, body, { headers });
+  }
+
+  deleteTask(id: string): Observable<any> {
+    const token = sessionStorage.getItem('token');
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+    return this.delete(`http://localhost:9200/api/tasks/${id}`, { headers });
   }
 }
